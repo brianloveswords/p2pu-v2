9c39acd47af3f48f9ab5991ec2f782042f3de5d6
Brian Brennan <brianloveswords@gmail.com>
2010-11-02 19:42:33 +0100

    THIS IS P2PU!!!

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    (r'^$/', include('p2pu.foo.urls)), 
+    # Example:
+    # (r'^p2pu/', include('p2pu_v2.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+)
